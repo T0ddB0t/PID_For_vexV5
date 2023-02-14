7c14d69505a2b15033c996a8d14627a10b5f8fb7
T0ddB0t <93291262+T0ddB0t@users.noreply.github.com>
2023-02-14 14:10:32 -0500

    it works kinda

--- PID.cc
+++ PID.cc
@@ -1,9 +1,9 @@
-      /*----------------------------------------------------------------------------*/
-     /*    Module:       main.cpp                                                  */
-    /*    Author:       Franky                                                    */
-   /*    Created:      Thu Sep 26 2022                                           */
-  /*    Description:  ARGGGGGGGGGGGGGGGGGG                                      */
- /*                                                                            */
+ /*----------------------------------------------------------------------------*/
+/*  Module:       main.cpp                                                    */
+/*    Author:       Franky                                                    */
+/*    Created:      Thu Sep 26 2022                                           */
+/*    Description:  ARGGGGGGGGGGGGGGGGGG                                      */
+/*                                                                            */
 /*----------------------------------------------------------------------------*/
 //
 // ---- START VEXCODE CONFIGURED DEVICES ----
@@ -13,82 +13,91 @@
 // LEFT                 encoder       C, D            
 // CENTER               encoder       E, F            
 // Drivetrain           drivetrain    1, 10, 11, 20   
+// TEST1                motor         2               
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
 #include <iostream>
 using namespace vex;
-
+using namespace std;
 // A global instance of competition
 competition Competition;
-//Code that the author wrote
-class pid{
-  public:
-    double out;
-    double out1;
-    double pre_er;
-    void pidFWBK(double sp, double pv, double Kp, double Ki, double Kd/*, double max, double min*/)
-    {    
-      //do i try a if statement to see if the LEFT/RIGHT encoders are equal?
-      //I would find the diffrence and add || subtract it to the error
-      if(LEFT.position(degrees) != RIGHT.position(degrees)){}
-     while(pv > sp){ 
-       double er = sp - pv;
-       double integ = 0;
-       integ += er;
-       double der = (er - pre_er);
-       //final step
-       double Pout = Kp * er;
-       double Iout = Ki * integ;
-       double Dout = Kd * der;
-       double uT = Pout + Iout - Dout;
-       // Makes the bot go back or forward
-        /*
-       L1.spinFor(forward, uT, degrees);
-       R1.spinFor(forward, -uT, degrees);
-       R2.spinFor(forward, -uT, degrees);
-       L2.spinFor(forward, uT, degrees);
-       */
-       Drivetrain.driveFor(uT, inches);
-       pre_er = er;
-      }
-    //wait(10, msec);
-   }
-    void pidSIDE(int sp, int pv, double Kp, double Ki, double Kd/*,double max, double min*/)
-    {
-      double er = sp - CENTER.position(degrees);
-      while(fabs(er) != sp)
-      {
-       double integ = 0;
-       integ += er;
-       double der = (er - pre_er);
-       //final step
-       double Pout = Kp * er;
-       double Iout = Ki * integ;
-       double Dout = Kd * der;
-       double uT = Pout + Iout - Dout;
-       //makes the bot turn
-       /*
-       R1.spinFor(forward, uT,degrees),
-       L2.spinFor(forward, uT,degrees),
-       L1.spinFor(forward, uT,degrees),
-       L2.spinFor(forward, uT,degrees);
-       */
-       Drivetrain.turnFor(uT, degrees);
-       pre_er = er;
-      }
-    }  
+// Code that the author wrote
+class pid {
+public:
+  double out;
+  double out1;
+  double pre_er;
+  void pidFWBK(double sp, double pv, double Kp, double Ki, double Kd /*, double max, double min*/) {
+    // do i try a if statement to see if the LEFT/RIGHT encoders are equal?
+    // I would find the diffrence and add || subtract it to the er
+    if (LEFT.position(degrees) != RIGHT.position(degrees)) {
+    }
+    while (pv > sp) {
+      double er = sp - pv;
+      double integ = 0;
+      integ += er;
+      double der = (er - pre_er);
+      // final step
+      double Pout = Kp * er;
+      double Iout = Ki * integ;
+      double Dout = Kd * der;
+      double uT = Pout + Iout - Dout;
+      // Makes the bot go back or forward
+      /*
+     L1.spinFor(forward, uT, degrees);
+     R1.spinFor(forward, -uT, degrees);
+     R2.spinFor(forward, -uT, degrees);
+     L2.spinFor(forward, uT, degrees);
+     */
+      Drivetrain.driveFor(uT, inches);
+      pre_er = er;
+    }
+
+  }
+  void pidSIDE(int sp, /*int pv*/ double Kp, double Ki, double Kd /*,double max, double min*/) {
+    double er = sp - CENTER.position(degrees);
+    double integ;
+    double der;
+    double uT = 0;
+    while (fabs(er) > uT) {
+      er = sp - CENTER.position(degrees);
+      integ = integ + er;
+      if(er == 0 || fabs(er) >= sp){
+			  integ = 0;
+		  }
+      der = (er - pre_er);
+      // final step
+      pre_er = er;
+      double Pout = Kp * er;
+      double Iout = Ki * integ;
+      double Dout = Kd * der;
+      double uT = Pout + Iout + Dout;
+      // makes the bot turn
+      /*
+      R1.spinFor(forward, uT,degrees),
+      L2.spinFor(forward, uT,degrees),
+      L1.spinFor(forward, uT,degrees),
+      L2.spinFor(forward, uT,degrees);
+      */
+
+      TEST1.spinFor(uT, degrees);
+      wait(15, msec);
+      //Drivetrain.turnFor(right, uT, degrees);
+      
+    }
+  }
 };
 // define your global instances of motors and other devices here
 
-        /*---------------------------------------------------------------------------*/
-       /*                          Pre-Autonomous Functions                         */
-      /*                                                                           */
-     /*  You may want to perform some actions before the competition starts.      */
-    /*  Do them in the following function.  You must return from this function   */
-   /*  or the autonomous and usercontrol tasks will not be started.  This       */
-  /*  function is only called once after the V5 has been powered on and        */
- /*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
 /*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
@@ -96,51 +105,51 @@ void pre_auton(void) {
   vexcodeInit();
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
-  LEFT.setPosition(0,degrees);
-  RIGHT.setPosition(0,degrees);
-  CENTER.setPosition(0,degrees);
+  LEFT.setPosition(0, degrees);
+  RIGHT.setPosition(0, degrees);
+  CENTER.setPosition(0, degrees);
 }
 
-        /*---------------------------------------------------------------------------*/
-       /*                                                                           */
-      /*                              Autonomous Task                              */
-     /*                                                                           */
-    /*  This task is used to control your robot during the autonomous phase of   */
-   /*  a VEX Competition.                                                       */
-  /*                                                                           */
- /*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
 void auton(void) {
   // ..........................................................................
   // Insert autonomous user code here.
-  // ..........................................................................  
- 
-  //pidFW,sp,pv,Kp,Ki,Kd,max,min)
-  //pidSI,sp,pv,Kp,Ki,Kd,max,min)
+  // ..........................................................................
+
+  // pidFW,sp,pv,Kp,Ki,Kd,max,min)
+  // pidSI,sp,pv,Kp,Ki,Kd,max,min)
   pid p;
-  //problem is prob because it doesnt have a thing to check wether or not it
-  //done executing like the drivetrain class functions
-  //p.pidFWBK(10, 0, .1, .001, 5.5);
-  p.pidSIDE(10, 0, .1, .001, 5.5);
-  //p.pidFWBK(10, 0, .1, .001, 5.5);
-  Brain.Screen.print(LEFT.position(degrees));
+  // problem is prob because it doesnt have a thing to check wether or not it
+  // done executing like the drivetrain class functions
+  // p.pidFWBK(10, 0, .1, .001, 5.5);
+  p.pidSIDE(90, .1, .001, 5.5);
+  //TEST1.spinFor(90, degrees);
+  //Drivetrain.turnFor(left, 90, degrees);
+  // p.pidFWBK(10, 0, .1, .001, 5.5);
   while (1) {
-     wait(20, msec); // Sleep the task for a short amount of time to
+    wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
   }
 }
-     ////////////////////
-    //  box of info   //
-   //                //
-  //                //
- // driver stuff   //
+////////////////////
+//  box of info   //
+//                //
+//                //
+// driver stuff   //
 ////////////////////
 void usercontrol(void) {
   // User control code here, inside the loop
-
   while (1) {
-    wait(20, msec); // Sleep the task for a short amount of time to                    // prevent wasted resources.
+    wait(20, msec); // Sleep the task for a short amount of time to prevent wasted resources.
   }
 }
 
@@ -150,10 +159,12 @@ int main() {
   Competition.autonomous(auton);
   Competition.drivercontrol(usercontrol);
   // Run the pre-autonomous function.
-  pre_auton();
+  //usercontrol();
+  //pre_auton();
   auton();
+  
   // Prevent main from exiting with an infinite loop.
   while (true) {
-   wait(100, msec); 
+    wait(100, msec);
   }
 }
